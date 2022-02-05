5de22e31661384716ad8ed7ebf511883167e8777
VirDeveloper <99087168+VirDeveloper@users.noreply.github.com>
2022-02-05 23:40:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
-
-  <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="description" content="{{ .Page.Params.description | default .Site.Params.description | markdownify }}">
-<meta name="author" content="{{ .Site.Params.authors }}">
-<meta name="generator" content="Hugo {{ hugo.Version }}">
-
-<meta name="docsearch:language" content="en">
-<meta name="docsearch:version" content="{{ .Site.Params.docs_version }}">
-
-<title>{{ if .IsHome }}{{ .Site.Title | markdownify }} · {{ .Site.Params.description | markdownify }}{{ else }}{{ .Title | markdownify }} · {{ .Site.Title | markdownify }} v{{ .Site.Params.docs_version }}{{ end }}</title>
-
-<link rel="canonical" href="{{ .Permalink }}">
-
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <link rel="shortcut icon" href="./assets/img/favicon.ico" />
+    <link
+      rel="apple-touch-icon"
+      sizes="76x76"
+      href="./assets/img/apple-icon.png"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/gh/creativetimofficial/tailwind-starter-kit/compiled-tailwind.min.css"
+    />
+    <title>Test</title>
+  </head><body>
 <a
                 href="/Profile.html"
                 class="text-gray-600 hover:text-gray-900"
@@ -20,7 +27,5 @@
                 href="landing.html"
                 class="text-gray-600 hover:text-gray-900"
                 >Creative Tim</a
-              >.
-  <?php
-  echo "sa";
-  ?>
+              >.</body>
+</html>
